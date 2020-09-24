1bb3b14d4b5aa1bc695adaa13fcd9e7fe46d56bf
Inga <ijaanimets@gmail.com>
2020-09-24 04:42:00 +0000

    Automatically backed up by Learn

--- lib/intro_to_ruby_hashes_lab.rb
+++ lib/intro_to_ruby_hashes_lab.rb
@@ -3,7 +3,7 @@ def new_hash
 end
 
 def my_hash
-  # return a valid hash with any key/value pair of your choice
+  dress = { color: pink, fabric: polyester, zipper: centerfront }
 end
 
 def pioneer
